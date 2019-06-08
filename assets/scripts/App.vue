bdf7fa63ebf9ebefc16b6ce963de1a188e30ccc5
Shoko Shoko <fruitmachine0@gmail.com>
2019-06-08 15:42:25 +0900

    インデンテーション設定できる

--- assets/scripts/App.vue
+++ assets/scripts/App.vue
@@ -5,6 +5,15 @@ section.j-container
     v-model='input'
     placeholder='Input text here!'
     )
+    .j-panel.mt-20
+      h2.j-panel__title Options
+      el-form(label-width='150px')
+        el-form-item(label='Indentation')
+          el-radio-group(v-model='indent')
+            el-radio(label='tab') Tab
+            el-radio(label='space') Space
+        el-form-item(label='Number of space' v-if='indent === "space"')
+          el-input-number(v-model='space' controls-position='right' size='small')
   .j-container__col
     .j-display(:class='{ error: !isValid }')
       template(v-if='isValid') {{ formattedData }}
@@ -16,7 +25,9 @@ export default {
   data() {
     return {
       input: '',
-      isValid: true
+      isValid: true,
+      indent: 'tab',
+      space: 2
     }
   },
   computed: {
@@ -24,10 +35,14 @@ export default {
       if (!this.input) return
       try {
         const json = JSON.parse(this.input)
-        return JSON.stringify(json, null, 4)
+        return JSON.stringify(json, null, this.indentation)
       } catch (e) {
         return false
       }
+    },
+    indentation() {
+      if (this.indent === 'tab') return '\t'
+      return this.space
     }
   },
   watch: {
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,22 @@
 global.$ = require('jquery')
 import Vue from 'vue'
-import { Input } from 'element-ui'
+import {
+  Form,
+  FormItem,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup
+} from 'element-ui'
 import App from './App.vue'
 import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.component(Form.name, Form)
+Vue.component(FormItem.name, FormItem)
 Vue.component(Input.name, Input)
+Vue.component(InputNumber.name, InputNumber)
+Vue.component(Radio.name, Radio)
+Vue.component(RadioGroup.name, RadioGroup)
 $(function() {
   var app = new Vue({
     el: '#json-formatter',
